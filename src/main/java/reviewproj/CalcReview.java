13fb9710c4c2355e69bd9252a317f06bd92a1431
Administrator <Administrator@305-09>
2016-05-02 09:44:53 +0900

    커밋커밋

--- src/main/java/reviewproj/CalcReview.java
+++ src/main/java/reviewproj/CalcReview.java
@@ -4,7 +4,7 @@ public class CalcReview {
 	private int result=0;
 	
 	public void add(int x, int y) {
-		result=x+y; //Ä¿¹ÔÇØº¸±â
+		result=x+y; //Ä¿¹ÔÇØº¸±â.
 	}
 	public int getResult() {
 		return result;
